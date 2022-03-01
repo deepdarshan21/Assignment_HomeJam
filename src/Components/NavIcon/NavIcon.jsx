4a604090e9f79fe557cd23bd9871f294750ed587
Deepdarshan <deepdarshan21@gmail.com>
2022-03-01 07:26:57 +0530

    Add Nav's Icon

---
+++ src/Components/NavIcon/NavIcon.jsx
@@ -0,0 +1,8 @@
+import "./NavIcon.css";
+import Logo from "../../Assets/logo.png";
+
+const NavIcon = () => {
+    return <img className="nav-icon" src={Logo} alt="HOMEJAM"/>;
+};
+
+export default NavIcon;
