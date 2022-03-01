f7cfd7e4e00af46e352dc4de33a7beac2062ecc3
Deepdarshan <deepdarshan21@gmail.com>
2022-03-01 11:31:12 +0530

    Add Brand Info

---
+++ src/Layouts/Brand/Brand.jsx
@@ -0,0 +1,15 @@
+import "./Brand.css"
+
+const Brand = () => {
+    return (
+        <div className="brand">
+            <h1 className="brand-name">Cari Cari</h1>
+            <p className="brand-message">
+                Live from their sofa to yours. Get closer to your favorite articles, and never miss
+                out.
+            </p>
+        </div>
+    );
+};
+
+export default Brand;
